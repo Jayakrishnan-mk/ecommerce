cccb309b4c33adfff8e56e096fb328cc3b19e796
Jayakrishnan-mk <jayakrishnanmk800@gmail.com>
2025-09-16 21:49:49 +0530

    6a done. checkout header splitted from checkout page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,33 +1,13 @@
+import { CheckoutHeaderPage } from "./CheckoutHeaderPage";
 import "./CheckoutPage.css";
-import "./checkout-header.css";
+import "./CheckoutHeaderPage.css";
 
 export function CheckoutPage() {
   return (
     <>
       <title>Checkout</title>
 
-      <div class="checkout-header">
-        <div class="header-content">
-          <div class="checkout-header-left-section">
-            <a href="/">
-              <img class="logo" src="images/logo.png" />
-              <img class="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
-
-          <div class="checkout-header-middle-section">
-            Checkout (
-            <a class="return-to-home-link" href="/">
-              3 items
-            </a>
-            )
-          </div>
-
-          <div class="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <CheckoutHeaderPage />
 
       <div class="checkout-page">
         <div class="page-title">Review your order</div>
